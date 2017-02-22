b4b5f5ffc52233f043f50ff3f04d70c67da83b99
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-22 15:37:08 -0500

    more practice problems

---
+++ improve_snippet.rb
@@ -0,0 +1,9 @@
+# Improve this code snippet:
+# if (month == 12)
+#   true
+# else
+#   false
+# end
+
+if month == 12
+end
--- practice_problems.rb
+++ practice_problems.rb
@@ -1,6 +1,19 @@
+require 'pry'
+
 # Write a method that reverses the order of words in the following string:
 
 dwarves = "bashful doc dopey grumpy happy sleepy sneezy"
 
 dwarves = dwarves.split(' ').reverse().join(' ')
 puts dwarves
+
+
+# Improve this code snippet:
+# if (month == 12)
+#   true
+# else
+#   false
+# end
+
+if (month == 12)
+end
