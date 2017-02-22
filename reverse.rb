391f1b7b256bb7d9c4251940f84eaa0376a0ac33
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-22 15:02:19 -0500

    one answer to reversing string question

---
+++ reverse.rb
@@ -0,0 +1,6 @@
+# Write a method that reverses the order of words in the following string:
+
+dwarves = "bashful doc dopey grumpy happy sleepy sneezy"
+
+dwarves = dwarves.split(' ').reverse().join(' ')
+puts dwarves
