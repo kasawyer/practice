a90abc810b0d992a01261998fde0efc9e2167940
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-23 16:17:42 -0500

    more practice problems

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
+def longest(a1, a2)
+  (a1 + a2).chars.sort.uniq.join
+end
+
 # Write a method that reverses the order of words in the following string:
 
 dwarves = "bashful doc dopey grumpy happy sleepy sneezy"
