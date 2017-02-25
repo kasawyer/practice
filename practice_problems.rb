23005b057fff2283724005ee90e935a591cd09db
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-25 17:10:09 -0500

    more practice problems

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
+# Count the number of Duplicates
+#
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphanumeric characters, including digits, uppercase and lowercase alphabets.
+#
+# Example
+#
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabbcdeB" -> 2 # 'a' and 'b'
+# "indivisibility" -> 1 # 'i'
+# "Indivisibilities" -> 2 # 'i' and 's'
+# "aa11" -> 2 # 'a' and '1'
+
+#first pass at a solution
 def duplicate_count(text)
   if text != ""
     array = text.split("")
@@ -15,6 +29,12 @@ def duplicate_count(text)
   else
     return 0
   end
+end
+
+#more elegant solution
+def duplicate_count(text)
+  arr = text.downcase.split("")
+  arr.uniq.count { |n| arr.count(n) > 1 }
 end
 
 
