243ce6909cbf9364bb12973341015dead9b9a8c2
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-26 13:54:42 -0500

    more practice problems

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
+# Trolls are attacking your comment section!
+#
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+#
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+#
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+
+def disemvowel(str)
+  array = str.chars
+  array.each_with_index do |char, i|
+    if "aeiouAEIOU".include?(char)
+      array[i] =  nil
+    end
+  end
+
+  new_string = array.join
+  return new_string
+end
+
 # Well met with Fibonacci bigger brother, AKA Tribonacci.
 #
 # As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
