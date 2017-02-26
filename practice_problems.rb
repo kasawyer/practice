7cceb71fbb051c15a35a9b171b0240ed830b205e
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-26 13:34:50 -0500

    more practice problems

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,5 +1,29 @@
 require 'pry'
 
+# Well met with Fibonacci bigger brother, AKA Tribonacci.
+#
+# As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+#
+# So, if we are to start our Tribonacci sequence with [1,1,1] as a starting input (AKA signature), we have this sequence:
+#
+# [1,1,1,3,5,9,17,31,...]
+# But what if we started with [0,0,1] as a signature? As starting with [0,1] instead of [1,1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+#
+# [0,0,1,1,2,4,7,13,24,...]
+# Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+#
+# Signature will always contain 3 numbers; n will always be a non-negative number; if n==0, then return an empty array and be ready for anything else which is not clearly specified ;)
+#
+# If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+
+def tribonacci(signature,n)
+  for i in 3..n
+    signature[i] = signature[i-1] + signature[i-2] + signature[i-3]
+  end
+  return signature.slice(0, n)
+end
+
+
 # Count the number of Duplicates
 #
 # Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphanumeric characters, including digits, uppercase and lowercase alphabets.
