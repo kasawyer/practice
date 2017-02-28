ec42afd12376163d968db974a181c58b6e508a1f
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-02-28 18:59:47 -0500

    more practice problems

--- practice_problems.js
+++ practice_problems.js
@@ -1,3 +1,17 @@
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+//
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// first solution
+String.prototype.toJadenCase = function (string) {
+  var words = this.valueOf().split(' ');
+  for (var i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+  }
+  return words.join(' ').trim();
+};
+
+
 // Return the number (count) of vowels in the given string.
 
 //first pass at a solution
