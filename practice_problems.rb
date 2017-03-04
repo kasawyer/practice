79a83e79b705b78a0ede56bf23065d2176f3501f
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-03-04 17:38:05 -0500

    more practice problems

--- practice_problems.rb
+++ practice_problems.rb
@@ -1,5 +1,69 @@
 require 'pry'
 
+# Description
+#
+# Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different races will certainly be involved. Each race has a certain 'worth' when battling against others. On the side of good we have the following races, with their associated worth:
+#
+# Hobbits - 1
+# Men - 2
+# Elves - 3
+# Dwarves - 3
+# Eagles - 4
+# Wizards - 10
+# On the side of evil we have:
+#
+# Orcs - 1
+# Men - 2
+# Wargs - 2
+# Goblins - 2
+# Uruk Hai - 3
+# Trolls - 5
+# Wizards - 10
+# Although weather, location, supplies and valor play a part in any battle, if you add up the worth of the side of good and compare it with the worth of the side of evil, the side with the larger worth will tend to win.
+#
+# Thus, given the count of each of the races on the side of good, followed by the count of each of the races on the side of evil, determine which side wins.
+#
+# Input:
+#
+# The function will be given two parameters. Each parameter will be a string separated by a single space. Each string will contain the count of each race on the side of good and evil.
+#
+# The first parameter will contain the count of each race on the side of good in the following order:
+#
+# Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+# The second parameter will contain the count of each race on the side of evil in the following order:
+#
+# Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+# All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+#
+# Output:
+#
+# Return ""Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins, or "Battle Result: No victor on this battle field" if it ends in a tie.
+
+def goodVsEvil( good, evil )
+  goodWorthByIndex = [ 1, 2, 3, 3, 4, 10 ]
+  evilWorthByIndex = [ 1, 2, 2, 2, 3, 5, 10 ]
+  goodValue = getSum( good, goodWorthByIndex )
+  evilValue = getSum( evil, evilWorthByIndex )
+  if goodValue == evilValue
+    return "Battle Result: No victor on this battle field"
+  end
+  return goodValue > evilValue ? "Battle Result: Good triumphs over Evil" : "Battle Result: Evil eradicates all trace of Good"
+end
+
+def getSum( group, worth )
+  sum = 0
+  of = group.split( ' ' )
+  for i in 0..of.length - 1
+    if of[ i ].to_i > 0
+      for j in 1..of[ i ].to_i
+        sum += worth[ i ].to_i
+      end
+    end
+  end
+  return sum
+end
+
+
 # You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
 
 #first solution
