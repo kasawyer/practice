8fcc45e7f4dfb24b6e12a2e32eeec9f09d3a9644
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-03-06 18:16:35 -0500

    completed vowel count challenge

--- javascript/practice_problems.js
+++ javascript/practice_problems.js
@@ -17,23 +17,3 @@ String.prototype.toJadenCase = function () {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }).join(" ");
 };
-
-
-// Return the number (count) of vowels in the given string.
-
-//first pass at a solution
-function getCount(str) {
-  var vowelsCount = 0;
-  for (var i = 0; i <= str.length - 1; i++) {
-    if (str.charAt(i) == "a" || str.charAt(i) == "e" || str.charAt(i) == "i" || str.charAt(i) == "o" || str.charAt(i) == "u") {
-      vowelsCount += 1;
-    }
-  }
-  return vowelsCount;
-}
-
-//better solution
-function getCount(str) {
-  var vowelsCount = 0;
-  return str.replace(/[^aeiou]/gi, '').length;
-}
