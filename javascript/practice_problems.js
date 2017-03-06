120d8ca953bb2ae69d69b39067c4507a0ae36bb4
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-03-06 18:15:43 -0500

    completed get the middle letter challenge

--- javascript/practice_problems.js
+++ javascript/practice_problems.js
@@ -37,20 +37,3 @@ function getCount(str) {
   var vowelsCount = 0;
   return str.replace(/[^aeiou]/gi, '').length;
 }
-
-
-// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
-// Examples:
-// Kata.getMiddle("test") should return "es"
-// Kata.getMiddle("testing") should return "t"
-// Kata.getMiddle("middle") should return "dd"
-// Kata.getMiddle("A") should return "A"
-
-function getMiddle(s)
-{
-  if (s.length % 2 == 1) {
-    return s[s.length/2 - 0.5];
-  } else {
-    return s[s.length/2 - 1] + s[s.length/2];
-  }
-}
