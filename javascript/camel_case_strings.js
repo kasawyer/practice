309a3224acc7794380b2e243825dc04acf58e537
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2017-03-13 19:23:58 -0400

    completed convert string to camel case challenge

--- javascript/camel_case_strings.js
+++ javascript/camel_case_strings.js
@@ -9,6 +9,15 @@
 // toCamelCase("The_Stealth_Warrior")
 
 function toCamelCase(str){
+  str = str.split(/[-_]/);
+  for(var i = 1;i < str.length;i++){
+    str[i] = str[i].charAt(0).toUpperCase().concat(str[i].slice(1));
+  }
+  return str.join("");
+}
+
+
+function toCamelCase(str){
   let regExp=/[-_]\w/ig;
   return str.replace(regExp,function(match){
     return match.charAt(1).toUpperCase();
